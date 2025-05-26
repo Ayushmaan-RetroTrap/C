04984c8b2db7a54485e1c6b809efca32f6b5e7b2
Ayushmaan-RetroTrap <ayushmaansharma1506@gmail.com>
2025-05-26 15:12:35 +0530

    Unit Convertor

---
+++ Unit Conversion/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+
+    int choice = 0;
+    float m = 0.0f;
+    float cm = 0.0f;
+
+    printf("Unit Conversion Program\n1. Meters to Centimeters\n2. Centimeters to Meters\n");
+    printf("Choose any one of the options to continue(1 or 2): ");
+    scanf("%d", &choice);
+
+    if(choice == 1){
+        //Meters to Centimeters
+
+        printf("Enter the number: ");
+        scanf("%f", &m);
+
+        m = m * 100;
+
+        printf("%.2f Centimeters", m);
+    }    
+    else if (choice == 2){
+        //Centimeters to Meters
+
+        printf("Enter the number: ");
+        scanf("%f", &cm);
+
+        cm = cm / 100;
+
+        printf("%.2f Meters", cm);
+        
+    }
+    
+
+    return 0;
+}
